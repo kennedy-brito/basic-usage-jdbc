d2aab0bfbb1fcf060f152d3f73d61639a0e507c8
kennedyBJJ <116119627+kennedyBJJ@users.noreply.github.com>
2024-11-21 16:49:31 -0300

    Inserting data in the databse
    Para inserir dados no banco usamos um PreparedStatement.
    
    Ele recebe uma String de inserção e configurações adicionais, como se ele deve retornar as chaves geradas entre outras.
    
    A string da consulta pode usar o que chamamos de placeholders, eles são posições que podem ser substituídas por dados que devem ser inseridos.
    
    No caso, o placeholder é o "?" e para substituir usamos a função set<Tipo de dado>(<índice>, <valor>).
    
    EX> preparedStatement.setString(1, "Carlinhos"). Substitui a primeira interrogação pela string "Carlinhos"
    
    Para executar basta chamar o método "executeUpdate".
    
    NOTA: Para criar uma data que deve ser inserida se usa a classe Date do pacote java.sql.Date.

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,14 +2,58 @@ package application;
 
 import db.DB;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoField;
 
 public class Program {
     public static void main(String[] args) {
 
+        Connection conn = null;
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        PreparedStatement st = null;
+
+        try{
+            conn = DB.getConnection();
+
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                    + "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+                    + "VALUES "
+                    + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS
+            );
+
+            st.setString(1, "Carl Purple");
+            st.setString(2, "carl@gmail.com");
+            st.setDate(3, Date.valueOf(LocalDate.parse("22/04/1985", fmt)));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
+
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0){
+                ResultSet rs = st.getGeneratedKeys();
+                while(rs.next()){
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            }
+            else {
+                System.out.println("No row affected!");
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+
+            DB.closeStatement(st);
+            DB.closeConnection();
+        }
+    }
+
+    public static void select(){
         Connection conn = null;
 
         Statement st = null;
